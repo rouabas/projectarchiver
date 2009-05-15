56ebd7a03dc8bb64cb5be90c378c731b06bf35af
gregmoinat <gregmoinat@52ca43ea-3a40-11de-a81c-fde7d73ceb89>
2009-05-15 08:32:23 +0000

    Le dossier, le bon

--- ProjectArchiver/src/ch/heigvd/projectarchiver/client/utils/Authentification.java
+++ ProjectArchiver/src/ch/heigvd/projectarchiver/client/utils/Authentification.java
@@ -3,20 +3,28 @@ package ch.heigvd.projectarchiver.client.utils;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.gwtext.client.widgets.Panel;
+import com.gwtext.client.widgets.Window;
+import com.gwtext.client.widgets.portal.Portlet;
 
 public class Authentification extends VerticalPanel {
 	
 	public Authentification () {
 		setWidth("100%");
 		setHeight("100px");
+		setHorizontalAlignment(VerticalPanel.ALIGN_CENTER);
 		add(construireTitre());
+		add(construireContenu());
 	}
 	
 	public SimplePanel construireTitre () {
 		SimplePanel titre = new SimplePanel();
-		titre.add(new HTML("<H1>Consultation de projet</H1></BR><H2>Authentification</H2>"));
+		titre.add(new HTML("<H1>Consultation de projet</H1>"));
 		return titre;
 	}
-
-
+	
+	public Portlet construireContenu () {
+		Portlet portlet = new Portlet("test", "cool");
+	    return portlet;
+	}
 }
